f426a8389e93c6837a479c497874d0eb72c584ce
eliz <eliz.vitebsk@gmail.com>
2019-05-03 16:14:26 -0700

    view

--- application_4/ViewController.swift
+++ application_4/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var systemSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var answerTextField: UILabel!
+    
+    @IBAction func getAnswerTapped(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

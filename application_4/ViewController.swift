232875bcbb85f141613b3ac6102bf973c75e2a60
eliz <eliz.vitebsk@gmail.com>
2019-05-03 18:00:05 -0700

    last version

--- application_4/ViewController.swift
+++ application_4/ViewController.swift
@@ -10,18 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var typeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var systemSegmentedControl: UISegmentedControl!
     @IBOutlet weak var numberTextField: UITextField!
     @IBOutlet weak var answerTextField: UILabel!
     
+    var first = 0
+    
+    @IBAction func buttonFirstIsOn(_ sender: Any) {
+        view.backgroundColor = UIColor(patternImage:UIImage(named:"4_bg1.jpg")!)
+        first = 0
+    }
+    
+    @IBAction func buttonSecondIsOn(_ sender: Any) {
+        view.backgroundColor = UIColor(patternImage:UIImage(named:"4_bg2.jpg")!)
+        first = 1
+    }
+    
     @IBAction func getAnswerTapped(_ sender: Any) {
+        var answer : String
+        answer = ""
+        let number = numberTextField.text!
         
+        if first == 0 {
+            if systemSegmentedControl.selectedSegmentIndex == 0 {
+                let number_numb = Int(numberTextField.text!)!
+                answer = String(number_numb, radix: 2)
+            }
+            else
+                if systemSegmentedControl.selectedSegmentIndex == 1 {
+                    let number_numb = Int(numberTextField.text!)!
+                    answer = String(number_numb, radix: 8)
+                }
+                else {
+                    let number_numb = Int(numberTextField.text!)!
+                   // if number_numb == Int(number, radix: 16) {
+                    answer = String(number_numb, radix: 10)
+                    //}
+                }
+        }
+        else {
+            if systemSegmentedControl.selectedSegmentIndex == 0 {
+                if let new_number = Int(number, radix: 2) {
+                    answer = String(new_number)
+                }
+            }
+            else
+                if systemSegmentedControl.selectedSegmentIndex == 1 {
+                    if let new_number = Int(number, radix: 8) {
+                        answer = String(new_number)
+                    }
+                }
+                else {
+                        if let new_number = Int(number, radix: 16) {
+                            answer = String(new_number)
+                        }
+                    }
+                }
+        
+        answerTextField.text = answer
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
